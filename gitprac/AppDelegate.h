cb50bcaac3e3865dae7f61c7c34b5447a33b73dd
Adam Johnson <adam.github@outlook.com>
2015-05-06 14:40:36 -0500

    first commit

---
+++ gitprac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitprac
+//
+//  Created by Adam Johnson on 5/6/15.
+//  Copyright (c) 2015 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitprac/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitprac
+//
+//  Created by Adam Johnson on 5/6/15.
+//  Copyright (c) 2015 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
